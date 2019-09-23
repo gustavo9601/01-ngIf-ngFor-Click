73d8d0c4538089ad165a8a52939df022779f262b
gustavo9601 <tavo9601@gmail.com>
2019-09-22 21:57:38 -0500

    Conceptos Basicos Angular - ngIf ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+
+/*Importando los componentes creados*/
+import {HeaderComponent} from './components/header/header.component';
+import {BodyComponent} from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
+  /*Declaramos los componententes creados*/
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +22,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
